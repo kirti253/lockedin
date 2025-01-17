0a990f535ecaa3506655863e123e1275ffea9e82
Aditya Pant <pantanupam05@gmail.com>
2025-01-17 15:42:06 +0530

    delete test

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 const mongoose = require("mongoose");
 const { healthcheckrouter } = require("./routes/healthcheck");
 app.use("/", healthcheckrouter);
+app.use(express.urlencoded({ extended: true }));
 const { userRouter } = require("./routes/user");
 const { adminRouter } = require("./routes/admin");
 const { tasklistRouter } = require("./routes/tasklist");
@@ -15,17 +16,17 @@ app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/tasklist", tasklistRouter);
 (async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGO_URL || "mongodb://localhost:27017"
-    );
-    console.log("Database connected successfully");
+	try {
+		await mongoose.connect(
+			process.env.MONGO_URL || "mongodb://localhost:27017"
+		);
+		console.log("Database connected successfully");
 
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server started at http://localhost:${port}`);
-    });
-  } catch (err) {
-    console.error("Error starting server:", err);
-  }
+		const port = process.env.PORT || 3000;
+		app.listen(port, () => {
+			console.log(`Server started at http://localhost:${port}`);
+		});
+	} catch (err) {
+		console.error("Error starting server:", err);
+	}
 })();
